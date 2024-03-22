d9935928ec301bfca02d72241b96adae536243dc
Jeremy <thisismejeremy@gmail.com>
2024-03-21 22:43:32 -0400

    graded assignment 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,37 @@
+import "./styling.css";
+
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type Of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data, id) => {
+        if(data.operational){
+          return(
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>active</td>
+          </tr>
+        )}
+        else{return(
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>inactive</td>
+          </tr>
+        )}
+      })}
      </tbody>
    </table>
   );
